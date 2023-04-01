3c54b10f518a0cc7658335e5d29cdd01eb89bf71
GabrielCarvalhoV <serumanovelho@gmail.com>
2023-04-01 10:54:34 -0300

    projeto finalizado

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 const controle = document.querySelectorAll("[data-controle]");
+const estatisticas = document.querySelectorAll("[data-estatistica]")
 const pecas = {
     "bracos": {
         "forca": 29,
@@ -32,18 +33,25 @@ const pecas = {
     },
 }
 
-controle.forEach( (elemento) => {
+controle.forEach((elemento) => {
     elemento.addEventListener("click", (evento) => {
         manipulaDados(evento.target.dataset.controle, evento.target.parentNode)
+        atualizaEstatisticas(evento.target.dataset.peca)
     })
 });
 
 function manipulaDados(operacao, controle) {
     const peca = controle.querySelector("[data-contador]");
-    
-    if(operacao === "-") {
+
+    if (operacao === "-") {
         peca.value = parseInt(peca.value) - 1;
-    }else{
+    } else {
         peca.value = parseInt(peca.value) + 1;
     }
+}
+
+function atualizaEstatisticas(peca) {
+    estatisticas.forEach( (elemento) => {
+        elemento.textContent = parseInt(elemento.textContent) + pecas[peca][elemento.dataset.estatistica]
+    })
 }
